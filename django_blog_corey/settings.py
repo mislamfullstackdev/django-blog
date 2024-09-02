ef5e5029085e30a2d188f50ca0375d35496b9542
mislamfullstackdev <mislam.pasona@gmail.com>
2024-09-02 16:53:19 +0900

    add: password reset and email send

--- django_blog_corey/settings.py
+++ django_blog_corey/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,16 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 # login URL
 LOGIN_URL = 'login'
+
+# password reset email send for development
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'  # Change 'sent_emails' to your preferred directory
+
+#email details for reset password for production
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('USER_NAME')
+# EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
